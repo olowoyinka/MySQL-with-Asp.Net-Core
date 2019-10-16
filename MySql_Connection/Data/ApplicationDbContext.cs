b00e33e594e906f71dd9841c28e6f18fad69f1a6
olowoyinka <olowofelayinka@gmail.com>
2019-10-16 09:34:57 +0100

    Done with the Project

---
+++ MySql_Connection/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using MySql_Connection.Models;
+
+namespace MySql_Connection.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MySql_Connection.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ MySql_Connection/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySql_Connection.Models
+{
+    public class Movie
+    {
+        public int MovieID { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
